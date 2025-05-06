5a5cf61d4cc202a67f3f1be419f218be49d68f4a
Luca Leon Happel <lucahappel99@gmx.de>
2025-05-06 21:16:59 +0200

    add: nimble service test code

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,9 @@
+#include "NimBLEDevice.h"
 #include "esp_log.h"
 #include "keyboard.hpp"
 #include "keyboard_button.h"
+#include <vector>
+#include "hal/gpio_types.h"
 #include <vector>
 
 extern "C" void app_main(void) {
@@ -9,4 +12,10 @@ extern "C" void app_main(void) {
   static std::vector<int> output_gpios = {0, 1, 2, 3, 4, 5, 6};
   static std::vector<int> input_gpios = {10, 11, 12, 13};
   static Keyboard keyboard(output_gpios, input_gpios);
+  // Code below this line in app_main will not be reached
+  ESP_LOGI("main", "Scan loop finished (should not happen)");
+  ESP_LOGI("main", "Starting esp32-corne-v3");
+  NimBLEDevice::init("ESP32-CORNE-V3");
+  NimBLEServer *pServer = NimBLEDevice::createServer();
+  NimBLEService *pService = pServer->createService("test-service");
 }
