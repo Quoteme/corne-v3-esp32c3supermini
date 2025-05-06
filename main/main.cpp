c72578ad27dead0ccc3814bf85f7564deb643a45
Luca Leon Happel <lucahappel99@gmx.de>
2025-05-06 19:44:40 +0200

    remove: legacy code

--- main/main.cpp
+++ main/main.cpp
@@ -2,17 +2,7 @@
 #include "hal/gpio_types.h"
 #include <vector>
 
-// *** TODO: IMPORTANT! Define the ESP32 GPIO pins you connected here ***
-const std::vector<gpio_num_t> connected_gpios = {
-    GPIO_NUM_1,  GPIO_NUM_2,  GPIO_NUM_3, GPIO_NUM_4, GPIO_NUM_5,
-    GPIO_NUM_6,  GPIO_NUM_7,  GPIO_NUM_8, GPIO_NUM_9, GPIO_NUM_10,
-    GPIO_NUM_11, GPIO_NUM_12, GPIO_NUM_13
-    // Add or remove pins according to your actual wiring
-};
-
 extern "C" void app_main(void) {
-  ESP_LOGI("main", "Starting C++ Key Finder Application");
-
   // Code below this line in app_main will not be reached
   ESP_LOGI("main", "Scan loop finished (should not happen)");
 }
