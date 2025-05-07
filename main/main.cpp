2b6aecf64593a6a9ba5658132c19188047fdc1a3
Luca Leon Happel <lucahappel99@gmx.de>
2025-05-07 12:56:16 +0200

    add: ble connect/disconnect callbacks

--- main/main.cpp
+++ main/main.cpp
@@ -1,32 +1,53 @@
 #include "NimBLEDevice.h"
+#include "NimBLEHIDDevice.h"
 #include "esp_log.h"
+#include "hal/gpio_types.h"
 #include "keyboard.hpp"
 #include "keyboard_button.h"
 #include <vector>
-#include "hal/gpio_types.h"
-#include <vector>
+
+class ServerCallbacks : public NimBLEServerCallbacks {
+  void onConnect(NimBLEServer *pServer, NimBLEConnInfo &connInfo) override {
+    printf("Client address: %s\n", connInfo.getAddress().toString().c_str());
+    pServer->updateConnParams(connInfo.getConnHandle(), 24, 48, 0, 180);
+  }
+
+  void onDisconnect(NimBLEServer *pServer, NimBLEConnInfo &connInfo,
+                    int reason) override {
+    printf("Client disconnected - start advertising\n");
+    NimBLEDevice::startAdvertising();
+  }
+} serverCallbacks;
+;
 
 extern "C" void app_main(void) {
   // Code below this line in app_main will not be reached
-  ESP_LOGI("main", "Scan loop finished (should not happen)");
   static std::vector<int> output_gpios = {0, 1, 2, 3, 4, 5, 6};
   static std::vector<int> input_gpios = {10, 11, 12, 13};
   static Keyboard keyboard(output_gpios, input_gpios);
   // Code below this line in app_main will not be reached
-  ESP_LOGI("main", "Scan loop finished (should not happen)");
   ESP_LOGI("main", "Starting esp32-corne-v3");
   NimBLEDevice::init("ESP32-CORNE-V3");
+  NimBLEDevice::setSecurityAuth(
+      /*BLE_SM_PAIR_AUTHREQ_BOND | BLE_SM_PAIR_AUTHREQ_MITM |*/
+      BLE_SM_PAIR_AUTHREQ_SC);
+  NimBLEDevice::setSecurityRespKey(0000);
+
   NimBLEServer *pServer = NimBLEDevice::createServer();
+  pServer->setCallbacks(&serverCallbacks);
   NimBLEService *pService = pServer->createService("test-service");
   NimBLECharacteristic *pCharacteristic =
       pService->createCharacteristic("1234");
 
   // --- Advertising ---
 
-  NimBLEAdvertising *pAdvertising =
-      NimBLEDevice::getAdvertising(); // create advertising instance
-  pAdvertising->addServiceUUID(
-      "test-service");                    // advertise the UUID of our service
-  pAdvertising->setName("Luca-Keyboard"); // advertise the device name
-  pAdvertising->start();                  // start advertising
+  NimBLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->addServiceUUID("test-service");
+  pAdvertising->setName("Luca-Keyboard");
+  pAdvertising->setAdvertisingCompleteCallback(
+      [](auto adv) { ESP_LOGI("BLE", "Advertising complete"); });
+  pAdvertising->start();
+
+  // --- HID ---
+  // NimBLEHIDDevice hid = NimBLEHIDDevice(pServer);
 }
